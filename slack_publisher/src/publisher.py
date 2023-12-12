182557940241f11627252daf49a608bd34153923
Uzair <uzam.dev@gmail.com>
2023-12-12 21:30:44 +0500

    updates

--- slack_publisher/src/publisher.py
+++ slack_publisher/src/publisher.py
@@ -15,3 +15,13 @@ class SlackPublisher:
                 raise Exception(f"Slack API returned an error: {response.status_code} - {response.body}")
         except Exception as e:
             logger.error(e, exc_info=True)
+
+    def on_failure_callback(self, context: dict):
+        try:
+            task_id = context["task_instance"].task_id
+            dag_id = context["task_instance"].dag_id
+            response = self.webhook_client.send(text=f"Task {task_id} in DAG {dag_id} failed")
+            if response.status_code != 200:
+                raise Exception(f"Slack API returned an error: {response.status_code} - {response.body}")
+        except Exception as e:
+            logger.error(e, exc_info=True)
